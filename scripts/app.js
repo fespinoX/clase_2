16b38e16ac40c1b43a80e8fa7bac8e4336b3535c
Flor Espino <florencia.espino@gmail.com>
2021-11-17 20:17:58 -0300

    clase 4 - initial commit

--- scripts/app.js
+++ scripts/app.js
@@ -1,53 +1,10 @@
 var myapp = new Vue({
     el: '#app',
     data: {
-      tienePiojitos: true,
-      piojitosMsg: "Tiene piojitos",
-      modoOscuro: true,
-      mostrarMsg: true,
-      textBtn: "Ocultar mensaje",
-      michis: [
-        {
-            nombre: 'Zelda',
-            pelo: 'largo',
-            color: 'gris'
-        },
-        {
-            nombre: 'Minerva',
-            pelo: 'corto',
-            color: 'blanco y gris'
-        },
-        {
-            nombre: 'Lady',
-            pelo: 'corto',
-            color: 'tricolor'
-        },
-        {
-            nombre: 'Mariah',
-            pelo: 'largo',
-            color: 'carey'
-        },
-
-
-      ]
+        numeroDeClase: 4,
     },
     methods: {
-        changeTheme() {
-            if (this.modoOscuro) {
-                this.modoOscuro = false;
-            } else {
-                this.modoOscuro = true;
-            }
-        },
-        toggleMsg() {
-            if (this.mostrarMsg) {
-                this.mostrarMsg = false;
-                this.textBtn = "Mostrar mensaje";
-            } else {
-                this.mostrarMsg = true;
-                this.textBtn = "Ocultar mensaje";
-            }
-        }
+        
     },
     computed: {
 
